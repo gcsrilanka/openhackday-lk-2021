50f0f44a7799b4476ebf2f7c7c7a2009db2a5fb0
shanjathurshan <jathurshanm3@gmail.com>
2021-10-15 20:44:39 +0530

    Created Footer #11

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import './footer.css';
 
-const Footer = () => {
-    return (
-        <div>
-
+const Footer = () => (
+    <div>
+        <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+                <h2>OpenHackDay <br/> Sri Lanka</h2>
+            </div>
+            <div className="col-md-4">
+            <h5>Docs</h5>
+                <ul>
+                    <a href="/"><li>Tutorial</li></a>
+                </ul>
+            </div>
+            <div className="col-md-4">
+            <h5>Community</h5>
+                <ul>
+                <a href="/"><li>Stack Overflow</li></a>
+                <a href="/"><li>Discord</li></a>
+                <a href="/"> <li>Twitter</li></a>
+                </ul>
+            </div>
+          </div>
         </div>
-    )
-}
+        </div>
+        <div className="text-center py-3">
+        Copyright © 2021 OpenHackDay. Built with 🖤 in Sri Lanka.
+        </div>
+    </div>
+)
 
 export default Footer
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,3 +1,5 @@
+import Footer from "../../components/Footer/Footer"
+
 const HomePage = () => (
 	<div>
 		<h1>Hello</h1>
