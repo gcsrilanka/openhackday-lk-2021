cff01ff0c747b271be470cf2ad3e4eca4297954f
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-17 22:12:26 +0530

    added team member section

---
+++ src/components/Team/Team.tsx
@@ -0,0 +1,21 @@
+import classNames from './Team.module.scss'
+import teamMembers from './teamMembers'
+
+export default function Team() {
+	return (
+		<div className={classNames.wrapper}>
+			<div className={classNames.title}>Team</div>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.
+			</p>
+			<div className={classNames.teamWrapper}>
+				{teamMembers.map(item => (
+					<div className={classNames.avatar} key={item.id}>
+						<img src={item.img} alt={item.name} />
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,14 +2,16 @@ import { Container } from '@mui/material'
 import Banner from '../../components/Banner/Banner'
 import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
+import Team from '../../components/Team/Team'
 
 const HomePage = () => (
 	<div>
 		<Navbar />
 		<Container>
 			<Banner />
+			<Team />
 		</Container>
-		<Footer/>
+		<Footer />
 	</div>
 )
 
