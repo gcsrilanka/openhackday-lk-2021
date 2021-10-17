e6bfd5283796f53bbd973cc8ecaf55274c4e1f35
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-17 22:12:39 +0530

    add mockup data

---
+++ src/components/Team/teamMembers.js
@@ -0,0 +1,29 @@
+const teamMembers = [
+	{
+		id: 1,
+		name: 'John Doe',
+		img: 'https://i.pravatar.cc/300?img=1',
+	},
+	{
+		id: 2,
+		name: 'John Doe',
+		img: 'https://i.pravatar.cc/300?img=2',
+	},
+	{
+		id: 3,
+		name: 'John Doe',
+		img: 'https://i.pravatar.cc/300?img=3',
+	},
+	{
+		id: 4,
+		name: 'John Doe',
+		img: 'https://i.pravatar.cc/300?img=4',
+	},
+	{
+		id: 5,
+		name: 'John Doe',
+		img: 'https://i.pravatar.cc/300?img=5',
+	},
+]
+
+export default teamMembers
