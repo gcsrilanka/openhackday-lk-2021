2b254e13a34f82d9323982f46101c32e49d2c3cf
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-17 21:47:03 +0530

    added faq section component

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import classNames from './FAQ.module.scss'
+import faqData from './faqData'
 
-const FAQ = () => {
-    return (
-        <div>
-
-        </div>
-    )
+export default function FAQ() {
+	return (
+		<div className={classNames.wrapper}>
+			<div className={classNames.title}>FAQ</div>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.
+			</p>
+			<div className={classNames.cardWrapper}>
+				{faqData.map(item => (
+					<div className={classNames.card} key={item.id}>
+						<div className={classNames.question}>{item.question}</div>
+						<p className={classNames.answer}>{item.answer}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
-
-export default FAQ
