98fcbf98857ccfd809545e2e2994efe8a3a85cb6
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-17 19:25:49 +0530

    added sponsors component

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import classNames from './Sponsors.module.scss'
+import gceLogo from '../../assets/images/gc-srilanka-logoblack 1.png'
+import sliitLogo from '../../assets/images/logo-dark 1.png'
+import hfLogo from '../../assets/images/logo-hacktoberfest-full.f42e3b1 1.png'
+import doLogo from '../../assets/images/Vector.png'
 
-const Sponsors = () => {
-    return (
-        <div>
-
-        </div>
-    )
+export default function Sponsors() {
+	return (
+		<div className={classNames.wrapper}>
+			<h2 className={classNames.title}>Sponsors</h2>
+			<div className={classNames.logoWrapper}>
+				<img src={hfLogo} alt='Hacktoberfest' />
+				<img src={doLogo} alt='DigitalOcean' />
+				<img src={gceLogo} alt='Github Campus Experts' />
+				<img src={sliitLogo} alt='Sliit FOSS' />
+			</div>
+		</div>
+	)
 }
-
-export default Sponsors
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,14 +2,16 @@ import { Container } from '@mui/material'
 import Banner from '../../components/Banner/Banner'
 import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
+import Sponsors from '../../components/Sponsors/Sponsors'
 
 const HomePage = () => (
 	<div>
 		<Navbar />
 		<Container>
 			<Banner />
+			<Sponsors />
 		</Container>
-		<Footer/>
+		<Footer />
 	</div>
 )
 
