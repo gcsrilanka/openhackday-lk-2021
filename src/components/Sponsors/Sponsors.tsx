ff2545cc08650ae87e4483eb1b01f4d17f6721d5
shanjathurshan <jathurshanm3@gmail.com>
2021-10-17 19:21:41 +0530

    Fixed #21 - Feature: Create Sponsors #21

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,11 +1,34 @@
+import { Container, Grid } from '@mui/material'
 import React from 'react'
+import digitalocean from '../../assets/images/digitalocean.png'
+import gc from '../../assets/images/gc-srilanka.png'
+import foss from '../../assets/images/logo-sliit-foss.png'
+import hacktoberfest from '../../assets/images/logo-hacktoberfest.png'
+import '../../scss/sponsors.scss'
+import '../../scss/_variables.scss'
 
-const Sponsors = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
+const Sponsors = () => (
+    <div className="sponsors">
+        <Container>
+        <Grid container justifyContent="center">
+            <h1>Sponsor</h1>
+        </Grid>
+        <Grid container className="" alignItems="center" >
+            <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
+                    <img src={hacktoberfest} alt='logo-hacktoberfest' className="img-fluid" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
+                    <img src={digitalocean} alt='digitalocean' className="img-fluid" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
+                    <img src={gc} alt='gc-srilanka' className="img-fluid" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
+                    <img src={foss} alt='logo-sliit-foss' className="img-fluid" />
+            </Grid>
+        </Grid>
+        </Container>
+    </div>
+)
 
 export default Sponsors
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,16 +1,18 @@
-import { Container } from '@mui/material'
+// import { Container } from '@mui/material'
 import About from '../../components/About/About'
-import Banner from '../../components/Banner/Banner'
+// import Banner from '../../components/Banner/Banner'
 import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
+import Sponsors from '../../components/Sponsors/Sponsors'
 
 const HomePage = () => (
 	<div>
 		<Navbar />
-		<Container>
+		{/* <Container>
 			<Banner />
-		</Container>
+		</Container> */}
 		<About />
+		<Sponsors />
 		<Footer/>
 	</div>
 )
