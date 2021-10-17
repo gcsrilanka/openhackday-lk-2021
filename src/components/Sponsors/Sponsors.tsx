df2ce31b31f5aff1e782126f239a2fd4798f8abb
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-17 19:32:59 +0530

    replaced digital ocean logo from ui

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -3,7 +3,7 @@ import classNames from './Sponsors.module.scss'
 import gceLogo from '../../assets/images/gc-srilanka-logoblack 1.png'
 import sliitLogo from '../../assets/images/logo-dark 1.png'
 import hfLogo from '../../assets/images/logo-hacktoberfest-full.f42e3b1 1.png'
-import doLogo from '../../assets/images/Vector.png'
+import doLogo from '../../assets/images/Group.png'
 
 export default function Sponsors() {
 	return (
