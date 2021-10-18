ea5ec36166b7b9edca759e28502684070ed48711
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-18 13:09:13 +0530

    fixed naming issues

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import classNames from '../../scss/FAQ.module.scss'
+import '../../scss/faq.scss'
 import faqData from '../../data/faqs.json'
 
 export default function FAQ() {
 	return (
-		<div className={classNames.wrapper}>
-			<div className={classNames.title}>FAQ</div>
+		<div className='wrapper'>
+			<div className='title'>FAQ</div>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua.
 			</p>
-			<div className={classNames.cardWrapper}>
+			<div className='cardWrapper'>
 				{faqData.map(item => (
-					<div className={classNames.card} key={item.id}>
-						<div className={classNames.question}>{item.question}</div>
-						<p className={classNames.answer}>{item.answer}</p>
+					<div className='card' key={item.id}>
+						<div className='question'>{item.question}</div>
+						<p className='answer'>{item.answer}</p>
 					</div>
 				))}
 			</div>
