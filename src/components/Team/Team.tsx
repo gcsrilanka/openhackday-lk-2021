70e9ac9a33e358d071c320e31f33b90a64a8a082
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-18 12:58:09 +0530

    wrapped content in a container

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,21 +1,24 @@
-import '../../scss/team.scss'
+import { Container } from '@mui/material'
 import teamMembers from '../../data/team.json'
+import '../../scss/team.scss'
 
 export default function Team() {
 	return (
-		<div className='wrapper'>
-			<div className='title'>Team</div>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.
-			</p>
-			<div className='teamWrapper'>
-				{teamMembers.map(item => (
-					<div className='avatar' key={item.id}>
-						<img src={item.img} alt={item.name} />
-					</div>
-				))}
+		<Container>
+			<div className='wrapper'>
+				<div className='title'>Team</div>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.
+				</p>
+				<div className='teamWrapper'>
+					{teamMembers.map(item => (
+						<div className='avatar' key={item.id}>
+							<img src={item.img} alt={item.name} />
+						</div>
+					))}
+				</div>
 			</div>
-		</div>
+		</Container>
 	)
 }
