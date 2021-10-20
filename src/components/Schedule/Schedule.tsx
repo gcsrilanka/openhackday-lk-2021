098fddbedc07567136ec7ffa52fb8cc507a76e9a
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2021-10-20 13:10:56 +0530

    fix title styles

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,23 +1,26 @@
 import { Container, Grid } from '@mui/material'
 import React from 'react'
 import ScheduleCard from './ScheduleCard'
-import scheduleData from '../../data/schedule.json';
+import scheduleData from '../../data/schedule.json'
+import '../../scss/schedule.scss'
 
 const Schedule = () => (
-    <div className="">
-        <Container>
-        <Grid container justifyContent="center">
-            <h1>Schedule</h1>
-        </Grid>
-        <Grid container className="schedule">
-            {scheduleData.map(data => 
-            <ScheduleCard title={data.title}time={data.time} name={data.name} src={data.src} href={data.href} /> 
-            )}
-        </Grid>
-
-        
-        </Container>
-    </div>
+	<Container className='wrapper'>
+		<Grid container justifyContent='center' className='title'>
+			<h1>Schedule</h1>
+		</Grid>
+		<Grid container className='schedule'>
+			{scheduleData.map(data => (
+				<ScheduleCard
+					title={data.title}
+					time={data.time}
+					name={data.name}
+					src={data.src}
+					href={data.href}
+				/>
+			))}
+		</Grid>
+	</Container>
 )
 
 export default Schedule
